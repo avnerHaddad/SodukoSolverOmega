2e4161b8c944489830cea101016c6efb9b847bf7
Avner <avner788@gmail.com>
2022-12-25 19:41:45 +0200

    fiished class board, done with setup time to create the lexing algo!

--- Board.cs
+++ Board.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,29 @@ namespace SodukoSolverOmega
     internal class Board
     {
         private Cell[,] cells;
-
         
+        //setter/getter for the matrix as a whole
+        public Cell[,] Cells
+        {
+            get { return cells; }
+            set { cells = value; }
+        }
+
+        //adding an iterator for the matrix
+        public Cell this[int i, int j]
+        {
+            get { return cells[i, j]; }
+            set { cells[i, j] = value; }
+        }
+
+
         public Board()
         {
             this.cells = new Cell[9,9];
         }
+
         
+
 
 
     }
