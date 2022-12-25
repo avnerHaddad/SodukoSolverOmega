3c0a0b790812e91031a690f368b65e2bb8806d65
Avner <avner788@gmail.com>
2022-12-25 15:07:35 +0200

    added basic template for lexer class

--- Lexer.cs
+++ Lexer.cs
@@ -8,5 +8,26 @@ namespace SodukoSolverOmega
 {
     internal class Lexer
     {
+        private Board board;
+        private string boardTxt;
+        private iterator iterator;
+        //create an iterator for the string
+
+        public Lexer(string inputTxt)
+        {
+            boardTxt = inputTxt; ;
+            board = new Board();
+            //initialise iterator later
+        }
+
+        private void CreateBoard()
+        {
+
+        }
+        public Board getBoard()
+        {
+            CreateBoard();
+            return board;
+        }
     }
 }
