49adbe987151f4d98a59b12b54ceec1a9b103264
Avner <avner788@gmail.com>
2022-12-26 22:23:50 +0200

    added timer and removed from folder for now

--- Board.cs
+++ Board.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SodukoSolverOmega.SodukoEngine
+namespace SodukoSolverOmega
 {
     internal class Board
     {
--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SodukoSolverOmega;
-using SodukoSolverOmega.SodukoEngine;
 using System.Configuration;
 using static SodukoSolverOmega.IOManager;
+
+
+using System.Diagnostics;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var watch = new System.Diagnostics.Stopwatch();
 
         PrintText(ConfigurationManager.AppSettings["welcomeMsg"]);
         while (true)
@@ -15,9 +18,16 @@ internal class Program
             if (GetInput(ConfigurationManager.AppSettings["inputMsg"]).Equals("1"))
             {
                 string boardStr = GetInput(ConfigurationManager.AppSettings["enterBoardMsg"]);
+
+                watch.Start();
+
                 SodukoSolver solver = new SodukoSolver();
                 Board myboard = solver.solve(boardStr);
+
+                watch.Stop();
                 PrintSoduko(myboard);
+                Console.WriteLine(" ");
+                Console.WriteLine($"time : {watch.ElapsedMilliseconds} ms");
                 //create a board object with boardStr
                 //call the solve function on it
                 //print the solved board or error if unsolvable
