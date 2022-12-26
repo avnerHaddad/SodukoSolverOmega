6e61e132b7587c026711ec7aaa2592f6d1363840
Avner <avner788@gmail.com>
2022-12-26 12:04:51 +0200

    added an Exceptions class

---
+++ SodukoExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolverOmega
+{
+    internal class SodukoExceptions : Exception
+
+    {
+        public SodukoExceptions(string message) : base(message)
+        {
+        }
+    }
+    internal class MyCustomException : SodukoExceptions
+    {
+        public MyCustomException(string message) : base(message)
+        {
+        }
+    }
+
+}
