0777098a0f79937fe4d3f660fe17baa9a3016afe
Avner <avner788@gmail.com>
2023-01-04 18:56:57 +0200

    added an interface for input and output

---
+++ IO/I_InputOuput.cs
@@ -0,0 +1,17 @@
+﻿using SodukoSolverOmega.SodukoEngine.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolverOmega.IO
+{
+    internal interface I_InputOuput
+    {
+        void OutputText(string text);
+        string GetInput();
+        
+        void OutputBoard(Board board);
+    }
+}
