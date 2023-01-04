91accb07f3d1d2b41a0abcd7a21c20483d5a25cb
Avner <avner788@gmail.com>
2023-01-04 20:04:11 +0200

    implemented file interface

--- IO/ConsoleIO.cs
+++ IO/ConsoleIO.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using SodukoSolverOmega.Configuration.Consts;
+using SodukoSolverOmega.Configuration.Exceptions;
+using SodukoSolverOmega.SodukoEngine.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SodukoSolverOmega.IO
 {
-    internal class ConsoleIO
+    internal class ConsoleIO : I_InputOuput
     {
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+
+        //move this function to  a diferent class?
+        //maybe board to string
+        public void OutputBoard(Board board)
+        {
+            for (int i = 0; i < Consts.BOARD_HEIGHT; i++)
+            {
+                OutputText("\n");
+                for (int j = 0; j < Consts.BOARD_WIDTH; j++)
+                {
+                    OutputText(Convert.ToString(board[i, j].Value) + " ");
+                }
+            }
+        }
+
+        public void OutputText(string text)
+        {
+            Console.Write(text);
+        }
     }
 }
--- IO/FileIO.cs
+++ IO/FileIO.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using SodukoSolverOmega.Configuration.Exceptions;
+using SodukoSolverOmega.SodukoEngine.Objects;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SodukoSolverOmega.IO
 {
-    internal class FileIO
+    internal class FileIO : I_InputOuput
     {
+        private StreamReader _reader;
+        private StreamWriter _writer;
+        public FileIO(string Path)
+        {
+            _reader = new StreamReader(Path);
+            _writer = new StreamWriter(Path);
+        }
+
+        public void OutputBoard(Board board)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void OutputText(string text)
+        {
+            string PreAppend = _reader.ReadToEnd();
+            string NewText = PreAppend + text;
+            _writer.Write(NewText);
+            
+        }
+        public string GetInput()
+        {
+            return _reader.ReadToEnd();
+            
+        }
     }
 }
