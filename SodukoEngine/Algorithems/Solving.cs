939f51acd59c7641ad4539bfbd375088df6311b6
Avner <avner788@gmail.com>
2023-01-10 23:24:40 +0200

    added empty files to spread the other funcs

---
+++ SodukoEngine/Algorithems/Solving.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolverOmega.SodukoEngine.Algorithems
+{
+    internal class Solving
+    {
+    }
+}
