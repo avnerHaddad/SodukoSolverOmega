ac45599a0281e32f819f920e8a296666f77ccc6d
Avner <avner788@gmail.com>
2023-01-13 01:02:16 +0200

    added a consoleUi to interact with user

---
+++ UI/ConsoleUI.cs
@@ -0,0 +1,39 @@
+﻿using SodukoSolverOmega.Configuration.Consts;
+using SodukoSolverOmega.IO;
+using SodukoSolverOmega.SodukoEngine.Solvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolverOmega.UI
+{
+    internal class ConsoleUI : ISudokuUI
+    {
+        private ConsoleIO Console;
+        public ConsoleUI()
+        {
+            Console = new ConsoleIO();
+        }
+
+        public void StartUp()
+        {
+            Console.OutputText(Consts.welcomeMsg);
+        }
+
+        public void UI_Loop()
+        {
+            StartUp(); 
+            while (true)
+            {
+                Console.OutputText(Consts.inputMsg);
+                //get input
+                string input = Console.GetInput();
+                SodukoSolver solver = new SodukoSolver();
+                Console.OutputText(solver.Solve(input).ToString);
+                Console.OutputText(Consts.EndMsg);
+            }
+        }
+    }
+}
---
+++ UI/ISudokuUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolverOmega.UI
+{
+    internal interface ISudokuUI
+    {
+        
+        public void UI_Loop();
+        public void StartUp();
+
+        
+    }
+}
