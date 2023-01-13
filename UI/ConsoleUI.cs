231568abfe145002a831e64beb7f20d3594c4767
Avner <avner788@gmail.com>
2023-01-13 03:20:24 +0200

    cleaned up and documented sudoku ui

--- UI/ConsoleUI.cs
+++ UI/ConsoleUI.cs
@@ -17,22 +17,30 @@ namespace SodukoSolverOmega.UI
             Console = new ConsoleIO();
         }
 
+        //runs at start and prints a nice wewlcome message to user
         public void StartUp()
         {
             Console.OutputText(Consts.welcomeMsg);
         }
 
+        //main loop of the program
         public void UI_Loop()
         {
+            
             StartUp(); 
             while (true)
             {
+                //ask for input
                 Console.OutputText(Consts.inputMsg);
                 //get input
                 string input = Console.GetInput();
+                //use input to create, solve and print the board
                 SodukoSolver solver = new SodukoSolver();
                 Console.OutputText(solver.Solve(input).ToString);
+
+                //message after solving that tells user how to procceed
                 Console.OutputText(Consts.EndMsg);
+                //todo: get input to naviagate ui
             }
         }
     }
