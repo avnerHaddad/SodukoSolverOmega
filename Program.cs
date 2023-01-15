6b8318648cd7b90b2a17604ce8d9c1672cc636b6
Avner <avner788@gmail.com>
2023-01-15 19:59:53 +0200

    moved some solve logic to the SudokuSolver constructor

--- Program.cs
+++ Program.cs
@@ -9,40 +9,7 @@ using SodukoSolverOmega.IO;
 
 internal class Program
 {
-    public void SodukoMode()
-    {
-        var watch = new System.Diagnostics.Stopwatch();
 
-        PrintText(Consts.welcomeMsg);
-        while (true)
-        {
-            if (GetInput(Consts.inputMsg).Equals("1"))
-            {
-                string boardStr = GetInput(Consts.enterBoardMsg);
-
-                watch.Start();
-
-                SodukoSolver solver = new SodukoSolver();
-                Board myboard = solver.Solve(boardStr);
-
-                watch.Stop();
-                PrintSoduko(myboard);
-                Console.WriteLine(" ");
-                Console.WriteLine($"time : {watch.ElapsedMilliseconds} ms");
-                //create a board object with boardStr
-                //call the solve function on it
-                //print the solved board or error if unsolvable
-            }
-            else
-            {
-                break;
-                //close the program
-            }
-
-
-        }
-        return;
-    }       
 
     private static void Main(string[] args)
     {
@@ -58,3 +25,40 @@ internal class Program
         console.OutputText($"time : {watch.ElapsedMilliseconds} ms");
     }
 }
+/*
+public void SodukoMode()
+{
+    var watch = new System.Diagnostics.Stopwatch();
+
+    PrintText(Consts.welcomeMsg);
+    while (true)
+    {
+        if (GetInput(Consts.inputMsg).Equals("1"))
+        {
+            string boardStr = GetInput(Consts.enterBoardMsg);
+
+            watch.Start();
+
+            SodukoSolver solver = new SodukoSolver();
+            Board myboard = solver.Solve(boardStr);
+
+            watch.Stop();
+            PrintSoduko(myboard);
+            Console.WriteLine(" ");
+            Console.WriteLine($"time : {watch.ElapsedMilliseconds} ms");
+            //create a board object with boardStr
+            //call the solve function on it
+            //print the solved board or error if unsolvable
+        }
+        else
+        {
+            break;
+            //close the program
+        }
+
+
+    }
+    return;
+}     
+}
+*/
--- SodukoEngine/Solvers/SodukoSolver.cs
+++ SodukoEngine/Solvers/SodukoSolver.cs
@@ -17,21 +17,23 @@ namespace SodukoSolverOmega.SodukoEngine.Solvers
         private Board BoardToSolve;
 
 
-        public SodukoSolver()
+        public SodukoSolver(String boardText)
         {
             //initialise a lexer and an empty board
             lexer = new Lexer();
             BoardToSolve = new Board();
+            //get the board in a board format using the lexer
+            BoardToSolve = lexer.getBoard(boardText);
+            //check if the board is valid
+            if (!BoardToSolve.IsValidBoard()) BoardToSolve = null;
+             
+            //set up the first constrints
+            BoardToSolve.InitialiseConstarints();
         }
 
         public Board Solve(string boardText)
         {
-            //get the board in a board format using the lexer
-            BoardToSolve = lexer.getBoard(boardText);
-            //check if the board is valid
-            if (!BoardToSolve.IsValidBoard()) return null;
-            //set up the first constrints
-            BoardToSolve.InitialiseConstarints();
+            if (BoardToSolve == null) { return BoardToSolve;}
             //check if solved without resorting to bruteforcing
             if (BoardToSolve.IsSolved())
             {
